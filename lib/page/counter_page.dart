8efe00bb05fcc53cec8906a5ba6df52e413bc639
Justine <justinecarl612@gmail.com>
2026-01-09 16:19:45 +0800

    Added counter_provider

--- lib/page/counter_page.dart
+++ lib/page/counter_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_master_prologue/provider/counter_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CounterPage extends StatelessWidget {
+class CounterPage extends ConsumerWidget {
   const CounterPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterProvider);
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(child: Text('Counter Page')),
+      body: Center(child: Text(counter.toString())),
     );
   }
 }
