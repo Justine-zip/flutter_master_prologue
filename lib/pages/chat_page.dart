e917354506a7e94f6443a71ab5560ab887f63bf8
Justine <justinecarl612@gmail.com>
2026-01-11 16:58:55 +0800

    Transform chat to realtime

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -7,52 +7,77 @@ class ChatPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final chatAsync = ref.watch(chatAsyncProvider);
+    final chatAsync = ref.watch(chatRealtimeProvider);
 
     return SafeArea(
-      child: RefreshIndicator(
-        onRefresh: () => ref.read(chatAsyncProvider.notifier).refresh(),
-        child: Scaffold(
-          backgroundColor: Colors.grey[850],
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Text(
-                  'M I S S I N G E R',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
-                    color: Colors.white,
-                  ),
+      child: Scaffold(
+        backgroundColor: Colors.grey[850],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                'M.O.N.E.Y',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  color: Colors.white,
                 ),
               ),
-              chatAsync.when(
-                data:
-                    (chats) => Expanded(
-                      child: ListView.builder(
-                        itemCount: chats.length,
-                        itemBuilder: (context, index) {
-                          final chat = chats[index];
-                          return ListTile(
-                            title: Text(
-                              chat.sender,
-                              style: TextStyle(color: Colors.white),
+            ),
+            chatAsync.when(
+              data:
+                  (chats) => Expanded(
+                    child: ListView.builder(
+                      itemCount: chats.length,
+                      itemBuilder: (context, index) {
+                        final chat = chats[index];
+                        final isMe = chat.sender == 'Josh';
+
+                        return Align(
+                          alignment:
+                              isMe
+                                  ? Alignment.centerRight
+                                  : Alignment.centerLeft,
+                          widthFactor: 1,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 4,
+                              horizontal: 8,
                             ),
-                            subtitle: Text(
-                              chat.message,
-                              style: TextStyle(color: Colors.white),
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey[800],
                             ),
-                          );
-                        },
-                      ),
+                            child: Column(
+                              crossAxisAlignment:
+                                  isMe
+                                      ? CrossAxisAlignment.end
+                                      : CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  chat.sender,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  chat.message,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                error: (e, _) => Text(e.toString()),
-                loading: () => CircularProgressIndicator(),
-              ),
-            ],
-          ),
+                  ),
+              error: (e, _) => Text(e.toString()),
+              loading: () => CircularProgressIndicator(),
+            ),
+          ],
         ),
       ),
     );
--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,7 +1,24 @@
 import 'package:flutter_master_prologue/models/chat.dart';
 import 'package:flutter_master_prologue/notifiers/chat_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+//Manual AsyncProvider
 final chatAsyncProvider = AsyncNotifierProvider<ChatNotifier, List<Chat>>(
   ChatNotifier.new,
 );
+
+//Realtime StreamProvider
+final chatRealtimeProvider = StreamProvider<List<Chat>>((ref) {
+  final supabase = Supabase.instance.client;
+
+  ref.keepAlive();
+
+  return supabase
+      .from('chat')
+      .stream(primaryKey: ['id'])
+      .order('created_at', ascending: true)
+      .map((rows) {
+        return rows.map((e) => Chat.fromJson(e)).toList();
+      });
+});
